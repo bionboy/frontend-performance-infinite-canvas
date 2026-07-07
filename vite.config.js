7b93cb2669ecb76c8ca97afbc987c76254da80be
Luke Floden <luke.floden@gmail.com>
2026-07-07 11:00:16 -0400

    build (github): host on github pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
+    base: "/frontend-performance-infinite-canvas/",
     plugins: [react()],
 });
